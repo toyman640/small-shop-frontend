bb071974a836a03c9cc3bd08cde4fad6a4f04f54
toyman640 <falakoadebayor@gmail.com>
2023-12-06 14:55:07 +0100

    Create menu

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -2,17 +2,24 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 const NavMenu = () => (
-  <header>
-    <nav className="container d-flex-space">
+  <header className="Navigation">
+    <nav className="Container">
       <div className="d-flex-space g-10">
         <h2>My Logo</h2>
       </div>
-      <ul className="d-flex-space g-10">
-        <li><NavLink to="/">Home</NavLink></li>
-        <li><NavLink to="/login">Log In</NavLink></li>
-        <li><NavLink to="/shop">Products</NavLink></li>
-        <li><NavLink to="/cart">Cart</NavLink></li>
-      </ul>
+      <div className="NavLinks">
+        <div className="Hamburger">
+          <span className="Bar" />
+          <span className="Bar" />
+          <span className="Bar" />
+        </div>
+        <ul className="MenuList">
+          <li><NavLink to="/">Home</NavLink></li>
+          <li><NavLink to="/login">Log In</NavLink></li>
+          <li><NavLink to="/shop">Products</NavLink></li>
+          <li><NavLink to="/cart">Cart</NavLink></li>
+        </ul>
+      </div>
     </nav>
   </header>
 );
