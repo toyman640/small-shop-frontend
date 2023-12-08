d9d34caa0bbc072f106d4304345f7a623df9e28e
toyman640 <falakoadebayor@gmail.com>
2023-12-08 09:05:09 +0100

    Fix liners error in banner

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import Banner from './homeComponents/Banner';
 
 const Home = () => (
   <div>
+    <Banner />
     <h2>This is Home Component</h2>
   </div>
 );
--- src/components/homeComponents/Banner.js
+++ src/components/homeComponents/Banner.js
@@ -1,11 +1,9 @@
-import React from "react";
+import React from 'react';
 
-const Banner = () => {
-  return (
-    <div>
-
-    </div>
-  );
-};
+const Banner = () => (
+  <div>
+    <h2>I am the banner</h2>
+  </div>
+);
 
 export default Banner;
