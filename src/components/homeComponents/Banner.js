c564974d5807532c5f1431b46d82435f837e60d0
toyman640 <falakoadebayor@gmail.com>
2023-12-08 16:04:45 +0100

    Modify

--- src/components/homeComponents/Banner.js
+++ src/components/homeComponents/Banner.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Banner = () => (
   <div className="BannerText">
     <h2 className="MainWork">Milky way gadgets</h2>
-    <p className="Slogan">if it&apos;s invented, we have it.</p>
+    <p className="Slogan">if it&apos;s invented, we have it!!!</p>
   </div>
 );
 
