e0f1485e2368b9b6a520c51c5096ac47d8d09301
toyman640 <falakoadebayor@gmail.com>
2023-12-13 13:21:06 +0100

    Make images background images

--- src/components/homeComponents/LatestProduct.js
+++ src/components/homeComponents/LatestProduct.js
@@ -1,26 +1,73 @@
 import React from 'react';
-import img1 from '../../images/img-1.jpg';
-import img2 from '../../images/img-2.jpg';
-import img3 from '../../images/img-3.jpg';
-import img4 from '../../images/img-4.jpg';
-import img5 from '../../images/img-5.jpg';
-import img6 from '../../images/img-6.jpg';
-import img7 from '../../images/img-7.jpg';
-import img8 from '../../images/img-8.jpg';
+// import img1 from '../../images/img-1.jpg';
+// import img2 from '../../images/img-2.jpg';
+// import img3 from '../../images/img-3.jpg';
+// import img4 from '../../images/img-4.jpg';
+// import img5 from '../../images/img-5.jpg';
+// import img6 from '../../images/img-6.jpg';
+// import img7 from '../../images/img-7.jpg';
+// import img8 from '../../images/img-8.jpg';
 
 const LatestProducts = () => (
   <div className="LatestProduct">
     <p className="SectionTitle">Latest Collection</p>
     <hr className="TitleUnder" />
     <div className="ImageSection">
-      <img className="ProductImage1" src={img1} alt="Product 1" />
-      <img className="ProductImage2" src={img2} alt="Product 2" />
-      <img className="ProductImage3" src={img3} alt="Product 3" />
-      <img className="ProductImage4" src={img4} alt="Product 4" />
-      <img className="ProductImage5" src={img5} alt="Product 5" />
-      <img className="ProductImage6" src={img6} alt="Product 6" />
-      <img className="ProductImage7" src={img7} alt="Product 7" />
-      <img className="ProductImage8" src={img8} alt="Product 8" />
+      <div className="ProductImage1 Translate">
+        <div className="Image-desc1">
+          <p>Loud Boom</p>
+          <p>Loder than every than you can think of</p>
+        </div>
+      </div>
+      <div className="ProductImage2 Translate">
+        <div className="Image-desc1">
+          <p>Loud Boom</p>
+          <p>Loder than every than you can think of</p>
+        </div>
+      </div>
+      <div className="ProductImage3 Translate">
+        <div className="Image-desc1">
+          <p>Loud Boom</p>
+          <p>Loder than every than you can think of</p>
+        </div>
+      </div>
+      <div className="ProductImage4 Translate">
+        <div className="Image-desc1">
+          <p>Loud Boom</p>
+          <p>Loder than every than you can think of</p>
+        </div>
+      </div>
+      <div className="ProductImage5 Translate">
+        <div className="Image-desc1">
+          <p>Loud Boom</p>
+          <p>Loder than every than you can think of</p>
+        </div>
+      </div>
+      <div className="ProductImage6 Translate">
+        <div className="Image-desc1">
+          <p>Loud Boom</p>
+          <p>Loder than every than you can think of</p>
+        </div>
+      </div>
+      <div className="ProductImage7 Translate">
+        <div className="Image-desc1">
+          <p>Loud Boom</p>
+          <p>Loder than every than you can think of</p>
+        </div>
+      </div>
+      <div className="ProductImage8 Translate">
+        <div className="Image-desc1">
+          <p>Loud Boom</p>
+          <p>Loder than every than you can think of</p>
+        </div>
+      </div>
+      {/* <img className="ProductImage2 Translate" src={img2} alt="Product 2" />
+      <img className="ProductImage3 Translate" src={img3} alt="Product 3" />
+      <img className="ProductImage4 Translate" src={img4} alt="Product 4" />
+      <img className="ProductImage5 Translate" src={img5} alt="Product 5" />
+      <img className="ProductImage6 Translate" src={img6} alt="Product 6" />
+      <img className="ProductImage7 Translate" src={img7} alt="Product 7" />
+      <img className="ProductImage8 Translate" src={img8} alt="Product 8" /> */}
     </div>
   </div>
 );
