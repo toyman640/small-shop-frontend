5c1f29a95f00b0750ce82c53fef29442bfa8db47
toyman640 <falakoadebayor@gmail.com>
2023-12-19 14:58:26 +0100

    Complete footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,38 @@
 import React from 'react';
+import Inst from '../images/socials/instagram2.png';
+import Fb from '../images/socials/facebook.png';
+import Twit from '../images/socials/twitter.png';
 
-const Footer = () => (
-  <div className="FooterSection">
-    <p className="FooterTitle">Milky way gadgets</p>
-    <ul className="SocialGroup">
-      <li>Twitter</li>
-      <li>Instagram</li>
-      <li>Facebook</li>
-    </ul>
-  </div>
-);
+const Footer = () => {
+  const currentYear = new Date().getFullYear();
+  return (
+    <div className="FooterSection">
+      <p className="FooterTitle">Milky way gadgets</p>
+      <ul className="SocialGroup">
+        <li>
+          <a href="https://web.facebook.com/" target="_blank" rel="noreferrer">
+            <img className="SocialIcons" src={Fb} alt="Facebook logo" />
+          </a>
+        </li>
+        <li>
+          <a href="https://www.instagram.com/" target="_blank" rel="noreferrer">
+            <img className="SocialIcons" src={Inst} alt="Instagram logo" />
+          </a>
+        </li>
+        <li>
+          <a href="https://twitter.com/" target="_blank" rel="noreferrer">
+            <img className="SocialIcons" src={Twit} alt="Twitter logo" />
+          </a>
+        </li>
+      </ul>
+      <p className="CopyRight">
+        &#169;
+        {currentYear}
+        .
+        All right reserved.
+      </p>
+    </div>
+  );
+};
 
 export default Footer;
