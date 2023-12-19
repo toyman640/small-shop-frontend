9ae559e4fdaccd1840e1fdd2ffc7317d0f810e00
toyman640 <falakoadebayor@gmail.com>
2023-12-19 13:22:38 +0100

    Footer title styled

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => (
+  <div className="FooterSection">
+    <p className="FooterTitle">Milky way gadgets</p>
+    <ul className="SocialGroup">
+      <li>Twitter</li>
+      <li>Instagram</li>
+      <li>Facebook</li>
+    </ul>
+  </div>
+);
+
+export default Footer;
