a833f18c144c067f80d7147af0df0f471aebb570
toyman640 <falakoadebayor@gmail.com>
2023-12-27 13:29:46 +0100

    Structure the table

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -2,9 +2,49 @@ import React from 'react';
 import Footer from './Footer';
 
 const Cart = () => (
-  <div>
+  <div className="CartPage">
     <div className="CartSetion">
-      <h2>Why up</h2>
+      <h4 className="CartTitle">Cart Contents</h4>
+      <table className="Table">
+        <thead>
+          <th>Item</th>
+          <th>Quantity</th>
+          <th>Price P/Q</th>
+          <th>Price</th>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Iphone 12</td>
+            <td>1</td>
+            <td>$400</td>
+            <td>$400</td>
+          </tr>
+          <tr>
+            <td colSpan="4" aria-label="Separator" className="CartHr">
+              &nbsp;
+              <hr />
+            </td>
+          </tr>
+          <tr>
+            <td>Alien Ware</td>
+            <td>1</td>
+            <td>$1200</td>
+            <td>$1200</td>
+          </tr>
+          <tr>
+            <td colSpan="4" aria-label="Separator" className="CartHr">
+              &nbsp;
+              <hr />
+            </td>
+          </tr>
+          <tr>
+            <td>Spy pen 3</td>
+            <td>1</td>
+            <td>$85</td>
+            <td>$85</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
     <Footer />
   </div>
