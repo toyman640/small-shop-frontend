0e79ee99e16ccebc007966e0156ecd733717d82d
toyman640 <falakoadebayor@gmail.com>
2024-01-17 10:45:49 +0100

    Add reducer and store

---
+++ src/redux/categories/categories.js
@@ -0,0 +1,40 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const GET_CAT_URL = 'http://127.0.0.1:3000/api/v1/categories'
+
+const initialState = {
+  categories: [],
+};
+
+export const fetchCategories = createAsyncThunk('categories/fetchCategories', async () => {
+  try {
+    const response = await axios.get(GET_CAT_URL);
+    return response.data;
+  } catch (err) {
+    return err.message;
+  }
+});
+
+const CategoriesSlice = createSlice ({
+  name: 'categories',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchCategories.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(fetchCategories.fulfilled, (state, action) => {
+        state.loading = false;
+        state.categories = action.payload;
+      })
+      .addCase(fetchCategories.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      });
+  }
+})
+
+export default CategoriesSlice.reducer;
